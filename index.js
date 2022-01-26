ab9e90701f51118dd14ad5f2ad2f1d139fbe26b9
elexis <eebrown422@gmail.com>
2022-01-26 09:21:04 -0500

    push

---
+++ index.js
@@ -0,0 +1,26 @@
+//1
+
+const myArray = [];
+for (let i = 1; i < 6; i++){
+    myArray.push(i);
+    }
+
+    //2 
+
+    const myArray = [];
+for (let i = 1; i <= 9; i +=2) {
+  myArray.push(i);
+}
+
+//3 
+const myArray = [];
+for (let i = 9; i > 0; i -= 2){
+  myArray.push(i);
+}\
+//4
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i-=2){
+  myArray.push(i);
+}
